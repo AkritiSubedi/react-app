d462570733c7913198dbd844c5618e7a47bbfd71
AkritiSubedi <aakusubedi075@gmail.com>
2023-12-05 18:17:08 +0545

    react_component

---
+++ src/assets/Message.tsx
@@ -0,0 +1,12 @@
+// we use tsx for react components
+
+// pascalCasing
+function Message(){
+    const name = 'Akriti';
+    if (name)
+        return <h1>Hello {name} </h1>;
+    return <h1>Hello World</h1>
+
+}
+
+export default Message;
