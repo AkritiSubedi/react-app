8fd6735156ff1535a1764e6e3f44573541551afb
AkritiSubedi <aakusubedi075@gmail.com>
2023-12-10 18:59:40 +0545

    event_handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ListGroup from './components/ListGroup';
-  // Make sure to provide the correct path to ListGroup
+  // Make sure to provide correct path to ListGroup
 
 function App() {
   return (
